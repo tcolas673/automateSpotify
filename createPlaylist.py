3640d4e19f91ca7a9bc662991f80dd7f5e6f6e20
tcolas673 <tcolas673@gmail.com>
2020-02-12 06:53:21 -0500

    uri

--- createPlaylist.py
+++ createPlaylist.py
@@ -9,19 +9,58 @@
 import json
 import requests
 from secrets import spotify_user_id, spotify_token
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
 
 
 class CreatePlaylist:
     def __init__(self):
         self.user_id = spotify_user_id
         self.spotify_token = spotify_token
+        self.youtube_client = self.get_youtube_client()
 
     # Step 1: Log Into Youtube
     def get_youtube_client(self):
-        pass
+       os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+       api_service_name = "youtube"
+       api_version = "v3"
+       client_secrets_file = "client_secret.json"
+
+       scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+       flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+       credentials = flow.run_console()
+
+       youtube_client = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+
+       return youtube_client
+
     # Step 2: Grab Our Liked Videos
     def get_liked_vieos(self):
-        pass
+        request = self.youtube_client.videos().list(
+            part = "snippet,contentDetails,statistics",
+            myRating="like"
+        )
+        response = request.execute()
+
+        for item in response["items"]:
+            video_title = item["snippet"]["title"]
+            youtube_url = "https://www.youtube.com/watch?={}".format(item["id"])
+
+            video = youtube_dl.YoutubeDL({}).extract_info(youtube_url, download=False)
+            song_name = video["track"]
+            artist = video["artist"]
+
+            self.all_song_info[video_title] = {
+                "youtube_url":youtube_url,
+                "song_name": song_name,
+                "artist": artist,
+
+                "spotify_uri": self.get_spotify_uri(song_name, artist)
+            }
+
     # Step 3: Create A New Playlist
     def create_playlist(self):
         requrest_body = json.dumps({
@@ -50,7 +89,9 @@ class CreatePlaylist:
         #only use the first song
         uri =songs[0]["uri"]
         return uri
-        
+
     # Step 5: Add this song into the new Spotify Playlist
-    def add_song_to_playlist(self):
-        pass
+    def add_song_to_playlist(self): 
+        self.get_liked_vieos()
+
+        for song,info in self.all
